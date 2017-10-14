fd249003af3484a057b58bbf709eda551b1e7288
xuelangren001 <xuelangren001@163.com>
2017-10-14 18:33:05 +0800

    我的电脑

--- app/src/main/java/com/kuoweather/MainActivity.java
+++ app/src/main/java/com/kuoweather/MainActivity.java
@@ -7,7 +7,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    //我操你吗
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
